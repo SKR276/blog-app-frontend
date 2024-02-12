948e86a0c74b031c5352bcd23eba5e7da8ebcda9
SKR276 <sreeponnu06@gmail.com>
2024-02-12 14:18:41 +0530

    login service added

--- lib/services/userServices.dart
+++ lib/services/userServices.dart
@@ -4,6 +4,30 @@ import 'dart:math';
 import 'package:http/http.dart' as http;
 
 class UserServiceApi{
+  Future<dynamic> sendLoginData(
+      String email,String password)async{
+    var client=http.Client();
+    var apiUrl=Uri.parse("http://172.16.185.196:3001/api/userBlog/signin");
+
+    var response=await client.post(apiUrl,
+    headers: <String,String>{
+      "Content-Type": "application/json; charset=UTF-8"
+    },
+      body: jsonEncode(<String,String>{
+        "email":email,
+        "password":password
+      })
+    );
+    if(response.statusCode==200)
+      {
+        return json.decode(response.body);
+      }
+    else
+      {
+        throw Exception("Failed to send");
+      }
+  }
+  
   Future<dynamic> sendData(
       String name,String age,String address,String mobileNo,String pincode,String email,String password) async{
     var client=http.Client();
